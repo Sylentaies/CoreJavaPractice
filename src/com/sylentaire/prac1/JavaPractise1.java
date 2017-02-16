dbeaf64ca090cdbd727c8a951210e86dff68a25c
Sylentair <sean.dong.fullstack@gmail.com>
2017-02-15 20:00:18 -0500

    adding parc1

---
+++ src/com/sylentaire/prac1/JavaPractise1.java
@@ -0,0 +1,10 @@
+package com.sylentaire.prac1;
+
+import java.security.spec.DSAGenParameterSpec;
+
+public class JavaPractise1 {
+	public static void main(String[] args) {
+		System.out.println("Hello,world");
+	}
+
+}
