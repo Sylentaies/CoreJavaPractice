b7876ce0fa3804da41cd1dde612ad5252e886ae8
Sylentair <sean.dong.fullstack@gmail.com>
2017-02-15 20:15:28 -0500

    project added

--- src/com/sylentaire/prac1/JavaPractise1.java
+++ src/com/sylentaire/prac1/JavaPractise1.java
@@ -1,7 +1,5 @@
 package com.sylentaire.prac1;
 
-import java.security.spec.DSAGenParameterSpec;
-
 public class JavaPractise1 {
 	public static void main(String[] args) {
 		System.out.println("Hello,world");
